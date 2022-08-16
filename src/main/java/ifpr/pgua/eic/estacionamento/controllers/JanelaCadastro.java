05c91da70ca395277f6be98aa669b683b608f090
eufelipecostaaa <iameduardas@gmail.com>
2022-08-16 08:08:26 -0600

    atualizacaoAula1608

--- src/main/java/ifpr/pgua/eic/estacionamento/controllers/JanelaCadastro.java
+++ src/main/java/ifpr/pgua/eic/estacionamento/controllers/JanelaCadastro.java
@@ -1,16 +1,21 @@
 package ifpr.pgua.eic.estacionamento.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import ifpr.pgua.eic.estacionamento.App;
 import ifpr.pgua.eic.estacionamento.models.Estacionamento;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
-public class JanelaCadastro {
-    
-    //declaração dos componentes JavaFX. O nome do atributo
-    //deve ser igual a propriedade fx:id no FXML.
+public class JanelaCadastro implements Initializable {
+
+    // declaração dos componentes JavaFX. O nome do atributo
+    // deve ser igual a propriedade fx:id no FXML.
 
     @FXML
     private TextField tfPlaca;
@@ -19,7 +24,7 @@ public class JanelaCadastro {
     private TextField tfModelo;
 
     @FXML
-    private TextField tfFabricante;
+    private ComboBox<String> cbFabricante;
 
     @FXML
     private TextField tfCor;
@@ -29,45 +34,48 @@ public class JanelaCadastro {
 
     private Estacionamento estacionamento;
 
-    public JanelaCadastro(Estacionamento estacionamento){
+    public JanelaCadastro(Estacionamento estacionamento) {
         this.estacionamento = estacionamento;
     }
 
-
     @FXML
-    private void cadastrar(){
+    private void cadastrar() {
         String placa = tfPlaca.getText();
         String modelo = tfModelo.getText();
-        String fabricante = tfFabricante.getText();
+        String fabricante = cbFabricante.getValue();
         String cor = tfCor.getText();
         String sAnoFabricacao = tfAnoFabricacao.getText();
 
         int anoFabricacao = Integer.valueOf(sAnoFabricacao);
 
-        if(estacionamento.cadastraVeiculo(placa, modelo, fabricante, anoFabricacao, cor)){
-            Alert alert = new Alert(AlertType.INFORMATION,"Cadastro realizado");
+        if (estacionamento.cadastraVeiculo(placa, modelo, fabricante, anoFabricacao, cor)) {
+            Alert alert = new Alert(AlertType.INFORMATION, "Cadastro realizado");
             alert.showAndWait();
             limpar();
-        }else{
-            Alert alert = new Alert(AlertType.INFORMATION,"Cadastro não realizado");
-            alert.showAndWait();  
+        } else {
+            Alert alert = new Alert(AlertType.INFORMATION, "Cadastro não realizado");
+            alert.showAndWait();
         }
     }
 
     @FXML
-    private void cancelar(){
+    private void cancelar() {
         App.popScreen();
     }
 
-
-
-    private void limpar(){
+    private void limpar() {
         tfPlaca.clear();
         tfModelo.clear();
-        tfFabricante.clear();
+        cbFabricante.getSelectionModel().clearSelection();
         tfCor.clear();
         tfAnoFabricacao.clear();
     }
 
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        cbFabricante.getItems().add("Fiat");
+        cbFabricante.getItems().add("Volkswagen");
+
+    }
 
 }
